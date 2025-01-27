0e5b6abb91225b12c87db51cf4c0003bd9b48114
teigorme <igorteixeira354@gmail.com>
2025-01-27 16:08:15 +0100

    add lib helmet and cors

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,16 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { patchNestjsSwagger } from '@anatine/zod-nestjs';
+import helmet from 'helmet';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{ logger: ['log', 'warn', 'error']});
+  const app = await NestFactory.create(AppModule, { logger: ['log', 'warn', 'error'] });
   const globalPrefix = 'api';
 
   app.setGlobalPrefix(globalPrefix);
-
+  app.use(helmet());
+  app.enableCors("*");
   const config = new DocumentBuilder()
     .setTitle('API NestJS🐈')
     .setDescription('Docs gerada automaticatimente para a API')
